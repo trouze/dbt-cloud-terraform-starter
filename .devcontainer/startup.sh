dadeccf14bcdedb859758b4f65488671f9a918ad
trouze <tyler@tylerrouze.com>
2024-10-31 15:01:58 -0500

    fix case sensitivity issue in env vars

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,3 +1,8 @@
+# codespace secrets are forced all caps which conflicts with terraform
+export TF_VAR_dbt_account_id=$TF_VAR_DBT_ACCOUNT_ID
+export TF_VAR_dbt_token=$TF_VAR_DBT_TOKEN
+export TF_VAR_dbt_host_url=$TF_VAR_DBT_HOST_URL
+
 # write dbtcloud_global_connection (no links) to account_connections.tf
 dbtcloud-terraforming -a ${TF_VAR_dbt_account_id} -t ${TF_VAR_dbt_token} --host-url ${TF_VAR_dbt_host_url} generate --resource-types dbtcloud_global_connection > account_connections.tf 2>&1
 
