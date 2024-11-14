4817af6d0e6410651feb3b311441e517d4b7c895
trouze <tyler@tylerrouze.com>
2024-11-14 09:05:53 -0600

    fix

--- .devcontainer/databricks/initialize_startup.sh
+++ .devcontainer/databricks/initialize_startup.sh
@@ -2,6 +2,7 @@
 export TF_VAR_dbt_account_id=$TF_VAR_DBT_ACCOUNT_ID
 export TF_VAR_dbt_token=$TF_VAR_DBT_TOKEN
 export TF_VAR_dbt_host_url=$TF_VAR_DBT_HOST_URL
+export TF_VAR_dbt_project_id=$TF_VAR_DBT_PROJECT_ID
 
 git remote add origin https://github.com/${GITHUB_REPOSITORY}.git
 git fetch --unshallow && git pull origin main
