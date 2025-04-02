cc54f3ed27352e37a8b8a616f6bc667be43d6f0b
Tyler Rouze <tyler@tylerrouze.com>
2025-04-02 16:57:25 -0400

    Create gitlab_variables.tf

---
+++ modules/repository/gitlab_variables.tf
@@ -0,0 +1,9 @@
+variable "repository_data" {
+  description = "Repository configuration"
+  type = any
+}
+
+variable "project_id" {
+  description = "The ID of the project this repository is associated with"
+  type        = string
+}
